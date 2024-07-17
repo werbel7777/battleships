01f868afdbdbe4afd941bad3783518d7c8c18b11
Sebastian Wojciechowski <se.wojciechowski6@gmail.com>
2024-07-17 19:16:05 +0200

    to make single field yellow

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-const tables = document.getElementsByClassName("board")
+const tables = document.getElementsByClassName("board");
 
-Array.from(tables).forEach(table => {
-    for (let i = 0; i < 10; i++) {
-        const row = document.createElement("tr")
+Array.from(tables).forEach((table) => {
+  for (let i = 0; i < 10; i++) {
+    const row = document.createElement("tr");
 
-        for (let j = 0; j < 10; j++) {
-            const column = document.createElement("td")
+    for (let j = 0; j < 10; j++) {
+      const column = document.createElement("td");
+      column.addEventListener("click", () => {
+        column.style.backgroundColor = "yellow";
+      });
 
-            row.appendChild(column)
-            table.appendChild(row)
-        }
+      row.appendChild(column);
+      table.appendChild(row);
     }
-});
-
-tables[0].addEventListener("click", () => {
-  tables[0].style.backgroundColor = "yellow";
+  }
 });
