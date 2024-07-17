399865a74f6bdc7593522ecae4537d106e55d66e
Sebastian Wojciechowski <se.wojciechowski6@gmail.com>
2024-07-17 19:40:46 +0200

    to prepare for complex pointer handling

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const tables = document.getElementsByClassName("board");
+const handleTdClick = (pointerEvent) => {
+  pointerEvent.target.classList.add("ship");
+};
 
 Array.from(tables).forEach((table) => {
   for (let i = 0; i < 10; i++) {
     const row = document.createElement("tr");
 
     for (let j = 0; j < 10; j++) {
-      const column = document.createElement("td");
-      column.addEventListener("click", () => {
-        column.style.backgroundColor = "yellow";
-      });
+      const td = document.createElement("td");
 
-      row.appendChild(column);
+      td.addEventListener("click", handleTdClick);
+      row.appendChild(td);
       table.appendChild(row);
     }
   }
